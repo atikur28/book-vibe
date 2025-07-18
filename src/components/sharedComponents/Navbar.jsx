e70d9f5379933ecdd9bc607fb47218943397921c
atikur28 <atikur45683@gmail.com>
2025-07-18 20:55:32 +0600

    Navbar edited

--- src/components/sharedComponents/Navbar.jsx
+++ src/components/sharedComponents/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
     },
   ];
   return (
-    <div className="navbar px-5 lg:px-10 py-8">
-      <div className="navbar-start">
+    <div className="flex items-center justify-between px-3 lg:px-10 py-8">
+      <div className="flex items-center">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
@@ -60,19 +60,19 @@ const Navbar = () => {
         </div>
         <Link
           to="/"
-          className="font-bold text-2xl"
+          className="font-bold text-2xl lg:text-3xl"
           style={{ fontFamily: "'Roboto', sans-serif" }}
         >
           Book Vibe
         </Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
+      <div className="hidden lg:flex lg:justify-center lg:gap-5">
         {navLinks.map((navLink, idx) => (
           <NavLink
             key={idx}
             to={navLink.path}
             className={({ isActive }) =>
-              `mr-5 px-5 py-3 rounded-xl transition-colors duration-200 ${
+              `px-5 py-3 rounded-xl transition-colors duration-200 ${
                 isActive
                   ? `font-bold border border-[#23BE0A] text-[#23BE0A]`
                   : `border border-transparent text-gray-600 hover:text-[#23BE0A] hover:border-[#23BE0A]`
@@ -85,18 +85,18 @@ const Navbar = () => {
         ))}
       </div>
       <div
-        className="navbar-end"
+        className="flex items-center gap-2"
         style={{ fontFamily: "'Roboto', sans-serif" }}
       >
         <Link
           to="/sign-in"
-          className="mr-3 px-5 py-3 rounded-xl transition-colors duration-200 font-bold cursor-pointer bg-[#23BE0A] text-white"
+          className="mr-3 px-3 md:px-5 py-1.5 md:py-3 text-sm md:text-base rounded-xl transition-colors duration-200 font-bold cursor-pointer bg-[#23BE0A] text-white"
         >
           Sign In
         </Link>
         <Link
           to="/sign-up"
-          className="px-5 py-3 rounded-xl transition-colors duration-200 font-bold cursor-pointer bg-[#59C6D2] text-white"
+          className="px-5 py-3 rounded-xl transition-colors duration-200 font-bold cursor-pointer bg-[#59C6D2] text-white hidden md:flex"
         >
           Sign Up
         </Link>
